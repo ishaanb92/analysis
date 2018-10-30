42c12ba41b91658af9c8a180628edbd8c42a1103
kilgore92 <ishaan.bits92@gmail.com>
2018-10-30 18:11:22 +0100

    MNIST embedding plots

--- analyze_embeddings.py
+++ analyze_embeddings.py
@@ -3,6 +3,8 @@ import numpy as np
 import os
 from stats import *
 import math
+import pickle
+from argparse import ArgumentParser
 
 """
 Script to calculate stats/plots for 3-emb experiment
@@ -14,6 +16,7 @@ Script to calculate stats/plots for 3-emb experiment
 def build_parser():
     parser = ArgumentParser()
     parser.add_argument('--run',type=str,help='Experiment run #',required=True)
+    parser.add_argument('--dataset',type=str,help='Dataset to analyze',default='mnist',required=False)
     return parser.parse_args()
 
 def read_file(model,run):
@@ -86,53 +89,93 @@ def create_histogram(col,model,root_dir,mode='test'):
     plot_hist(col=col,fname=fname)
 
 
-def analyze_embeddings(run,draw=False,log_file=None):
+def analyze_embeddings(run,draw=False,log_file=None,dataset='mnist'):
 
-    root_dir = os.path.join(os.getcwd(),'viz','run_{}'.format(run),'embeddings')
+    root_dir = os.path.join(os.getcwd(),'viz',dataset,'run_{}'.format(run),'embeddings')
 
     if os.path.exists(root_dir) is False:
         os.makedirs(root_dir)
 
-    df_test,df_train,df_gap = concat_df(run)
+    # Code to plot embeddings for MNIST
+    if dataset == 'mnist':
+        fname = os.path.join(root_dir,'mnist_plot_dict.pkl')
+        with open(fname,'rb') as f:
+            try:
+                mnist_plot_dict = pickle.load(f)
+            except:
+                print('Error reading file,exiting')
+                return None,None,None
+        colors = ['#ff0000', '#ffff00', '#00ff00', '#00ffff', '#0000ff','#ff00ff', '#990000', '#999900', '#009900', '#009999']
+        groups = [str(i) for i in range(10)]
+        data = []
+        for number in groups:
+            data.append(mnist_plot_dict[int(number)])
 
-    dist_means_test,dist_var_test = calculate_embedding_stats(df_test)
-    dist_means_train,dist_var_train = calculate_embedding_stats(df_train)
+        data = tuple(data)
+        # Create plot
+        fig = plt.figure()
 
-    for model in models:
-        mean_gap = math.fabs(dist_means_test[model]-dist_means_train[model])
-        if log_file is not None:
-            log_file.write('{} :: test-gz mean distance : {} train-gz distance : {} mean gap : {} \n'.format(model.upper(),dist_means_test[model],dist_means_train[model],mean_gap))
-        else:
-            print('{} :: test-gz mean distance : {} train-gz distance : {} mean gap : {}'.format(model.upper(),dist_means_test[model],dist_means_train[model],mean_gap))
+        for embs,color,group in zip(data,colors,groups):
+            x= []
+            y = []
 
-    #Homogenity Checks
-    pairs = generate_pairs()
+            for emb in embs:
+                x.append(emb[0])
+                y.append(emb[1])
 
-    for pair in pairs:
-        if check_homegenity(df_test[pair[0]],df_test[pair[1]]) is True:
-            if log_file is not None:
-                log_file.write('Test-Gz distances computed for models {} and {} are homogenous\n'.format(pair[0].upper(),pair[1].upper()))
-            else:
-                print('Test-Gz distances computed for models {} and {} are homogenous'.format(pair[0].upper(),pair[1].upper()))
+            plt.plot(x, y,'.',c=color,label=group)
 
-        if check_homegenity(df_train[pair[0]],df_train[pair[1]]) is True:
-            if log_file is not None:
-                log_file.write('Train-Gz distances computed for models {} and {} are homogenous\n'.format(pair[0].upper(),pair[1].upper()))
-            else:
-                print('Train-Gz distances computed for models {} and {} are homogenous'.format(pair[0].upper(),pair[1].upper()))
+        plt.title('MNIST embeddings using Center Loss')
+        plt.legend()
+        plt.show()
+
+        return None,None,None,None
 
 
-    if draw is True:
-        create_box_plot(df=df_test,mode='test',root_dir=root_dir)
-        create_box_plot(df=df_train,mode='train',root_dir=root_dir)
-        create_box_plot(df=df_gap,mode='gap',root_dir=root_dir)
+
+
+
+    if dataset == 'celeba':
+        df_test,df_train,df_gap = concat_df(run)
+
+        dist_means_test,dist_var_test = calculate_embedding_stats(df_test)
+        dist_means_train,dist_var_train = calculate_embedding_stats(df_train)
+
         for model in models:
-            create_histogram(col=df_test[model],model=model,root_dir=root_dir,mode='test')
-            create_histogram(col=df_train[model],model=model,root_dir=root_dir,mode='train')
+            mean_gap = math.fabs(dist_means_test[model]-dist_means_train[model])
+            if log_file is not None:
+                log_file.write('{} :: test-gz mean distance : {} train-gz distance : {} mean gap : {} \n'.format(model.upper(),dist_means_test[model],dist_means_train[model],mean_gap))
+            else:
+                print('{} :: test-gz mean distance : {} train-gz distance : {} mean gap : {}'.format(model.upper(),dist_means_test[model],dist_means_train[model],mean_gap))
+
+        #Homogenity Checks
+        pairs = generate_pairs()
+
+        for pair in pairs:
+            if check_homegenity(df_test[pair[0]],df_test[pair[1]]) is True:
+                if log_file is not None:
+                    log_file.write('Test-Gz distances computed for models {} and {} are homogenous\n'.format(pair[0].upper(),pair[1].upper()))
+                else:
+                    print('Test-Gz distances computed for models {} and {} are homogenous'.format(pair[0].upper(),pair[1].upper()))
+
+            if check_homegenity(df_train[pair[0]],df_train[pair[1]]) is True:
+                if log_file is not None:
+                    log_file.write('Train-Gz distances computed for models {} and {} are homogenous\n'.format(pair[0].upper(),pair[1].upper()))
+                else:
+                    print('Train-Gz distances computed for models {} and {} are homogenous'.format(pair[0].upper(),pair[1].upper()))
+
+
+        if draw is True:
+            create_box_plot(df=df_test,mode='test',root_dir=root_dir)
+            create_box_plot(df=df_train,mode='train',root_dir=root_dir)
+            create_box_plot(df=df_gap,mode='gap',root_dir=root_dir)
+            for model in models:
+                create_histogram(col=df_test[model],model=model,root_dir=root_dir,mode='test')
+                create_histogram(col=df_train[model],model=model,root_dir=root_dir,mode='train')
 
 
 
-    return dist_means_test,dist_means_train,dist_var_test,dist_var_train
+        return dist_means_test,dist_means_train,dist_var_test,dist_var_train
 
 if __name__ == '__main__':
     args = build_parser()
--- plot_grads.py
+++ plot_grads.py
@@ -36,16 +36,16 @@ def plot_grads(grads,model,gp_term = False):
 
     grad_list = flatten_grads(grads)
 
-    plt.figure(figsize=(20,20))
+    plt.figure(figsize=(20,15))
 
     plt.plot(iters,grad_list)
 
     plt.xlabel('Training Iteration')
 
     if model != 'dcgan':
-        plt.title('{} : Gradient Norm used in Regularization term'.format(model.upper()),fontsize=30)
+        plt.title('{} : Gradient Norm used in Regularization term'.format(model.upper()),fontsize=40)
     else:
-        plt.title('NSGAN : Gradient Norm used in Regularization term',fontsize=30)
+        plt.title('NSGAN : Gradient Norm used in Regularization term',fontsize=40)
 
 
 
@@ -56,19 +56,19 @@ def plot_grads(grads,model,gp_term = False):
 
 
     if model == 'wgan':
-        plt.ylabel('Unregularized Critic Gradient Norm',fontsize=25)
+        plt.ylabel('Unregularized Critic Gradient Norm',fontsize=40)
     elif model == 'wgan-gp':
         plt.ylabel('Critic Gradient Norm')
     elif model == 'dcgan' or model == 'dcgan_sim':
-        plt.ylabel('Unregularized Discriminator Gradient Norm',fontsize=25)
+        plt.ylabel('Discriminator Gradient Norm',fontsize=40)
     else:
-        plt.ylabel('Discriminator Gradient Norm',fontsize=25)
+        plt.ylabel('Discriminator Gradient Norm',fontsize=40)
 
     if os.path.exists('grad_figures') is False:
         os.makedirs('grad_figures')
 
-    plt.tick_params(labelsize=25)
-    plt.xlabel('Training Iteration',fontsize=25)
+    plt.tick_params(labelsize=30)
+    plt.xlabel('Training Iteration',fontsize=40)
 
     if gp_term is False:
         filename = 'grads_{}'.format(str(model)) +'.png'
