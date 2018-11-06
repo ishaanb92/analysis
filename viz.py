a2dd2f30b7a86989dd703f0963664d74edf20137
kilgore92 <ishaan.bits92@gmail.com>
2018-11-06 22:37:02 +0100

    Visualizing the 3-vec experiment

--- viz.py
+++ viz.py
@@ -14,23 +14,25 @@ Script to create t-SNE viz for embeddings
 def build_parser():
     parser = ArgumentParser()
     parser.add_argument('--model',type=str,help='GAN model used to select the right dictionary',required=True)
-    parser.add_argument('--reduce',type=str,help='GAN model used to select the right dictionary',default='tsne')
+    parser.add_argument('--reduce',type=str,help='GAN model used to select the right dictionary',default='pca')
+    parser.add_argument('--dataset',type=str,help='mnist/celeba',default='celeba')
+    parser.add_argument('--run',type=str,help='Experiment Run',default='1')
     return parser.parse_args()
 
 def load_embeddings(args):
 
-    emb_dir =  os.path.join(os.getcwd(),'viz','embeddings')
+    emb_dir =  os.path.join(os.getcwd(),'viz',args.dataset,'run_{}'.format(args.run),'embeddings')
 
     # Load train
-    with open(os.path.join(os.getcwd(),'viz','{}_embedding'.format(args.model.upper()),'closest_train_emb.pkl'),'rb') as f:
+    with open(os.path.join(os.getcwd(),'viz',args.dataset,'run_{}'.format(args.run),'{}_{}_embedding'.format(args.model.upper(),args.dataset.upper()),'closest_train_emb.pkl'),'rb') as f:
         train_dict = pickle.load(f)
 
     # Load test
-    with open(os.path.join(emb_dir,'test_emb_dict.pkl'),'rb') as f:
+    with open(os.path.join(emb_dir,'test_{}_emb_dict.pkl'.format(args.dataset.lower())),'rb') as f:
         test_dict = pickle.load(f)
 
     # Load G(z) -- specific to model
-    with open(os.path.join(emb_dir,'{}_emb_dict.pkl'.format(args.model.lower())),'rb') as f:
+    with open(os.path.join(emb_dir,'{}_{}_emb_dict.pkl'.format(args.model.lower(),args.dataset.lower())),'rb') as f:
         gz_dict = pickle.load(f)
 
     return train_dict,test_dict,gz_dict
@@ -58,24 +60,37 @@ def create_plot(args):
     matrix,labels = create_matrix(args)
 
     # PCA on the matrix (Suggested :
-    pca = PCA(n_components = 50)
-    matrix_pca = pca.fit_transform(X=matrix)
+    if args.dataset == 'celeba':
+        pca = PCA(n_components = 50)
+        matrix_pca = pca.fit_transform(X=matrix)
 
-    image_path = os.path.join(os.getcwd(),'viz','{}_{}_pca_plot.png'.format(args.model.lower(),args.reduce))
+    image_path = os.path.join(os.getcwd(),'viz',args.dataset,'run_{}'.format(args.run),'3_vec_{}.png'.format(args.model.lower()))
 
     # Legend positioning
     fig, ax = plt.subplots(ncols=1)
     ax.legend(loc="upper left", bbox_to_anchor=(0.6,0.5))
 
-    hyp.plot(x=matrix_pca,
-            hue=labels,
-            legend = ['Closest Train Images','Test Image Embeddings','G(z) Image Embeddings'],
-            fmt='.',
-            ndims=2,
-            reduce=args.reduce.upper(),
-            title='{} plot for {}'.format(args.reduce.upper(),args.model.upper()),
-            save_path=image_path,
-            ax = ax)
+    if args.dataset == 'celeba':
+        hyp.plot(x=matrix_pca,
+                hue=labels,
+                legend = ['Closest Train Images','Test Image Embeddings','G(z) Image Embeddings'],
+                fmt='.',
+                ndims=2,
+                reduce=args.reduce.upper(),
+                title='{} plot for {}'.format(args.reduce.upper(),args.model.upper()),
+                save_path=image_path,
+                ax = ax)
+    else:
+        hyp.plot(x=matrix,
+                hue=labels,
+                legend = ['Closest Train Images','Test Image Embeddings','G(z) Image Embeddings'],
+                fmt='.',
+                ndims=2,
+                reduce=None,
+                title='3-vector plot for {}'.format(args.model.upper()),
+                save_path=image_path,
+                ax = ax)
+
 
 if __name__ == '__main__':
     args = build_parser()
